5fa2d428eb129ec9512af04c27a2c2af86bbe254
mvasilenko <mvasilenko@gmail.com>
2017-11-21 16:31:36 +0200

    tagging docker image by git commit hash

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 export DOCKER_HUB_USERNAME="mvasilenko"
-export HUGO_APP="mvasilenko-blog"
-docker login -u ${DOCKER_HUB_USERNAME}
-export HUGO_APP_TAG="1.0"
-docker build -t ${DOCKER_HUB_USERNAME}/${HUGO_APP}:${HUGO_APP_TAG} .
-docker push ${DOCKER_HUB_USERNAME}/${HUGO_APP}:${HUGO_APP_TAG}
+export APP="mvasilenko-blog"
+if grep -q 'auths": {}' ~/.docker/config.json ; then
+    docker login -u ${DOCKER_HUB_USERNAME}
+fi
+export TAG=$(git log -1 --format=%H)
+docker build -t ${DOCKER_HUB_USERNAME}/${APP}:${TAG} .
+docker tag ${DOCKER_HUB_USERNAME}/${APP}:${TAG} ${DOCKER_HUB_USERNAME}/${APP}:latest
+docker push ${DOCKER_HUB_USERNAME}/${APP}:latest
 #docker rmi $(docker images --filter=reference="${DOCKER_HUB_USERNAME}/${HUGO_APP}" -q)
